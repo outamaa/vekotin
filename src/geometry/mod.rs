6736a24790604c4a25060980f3ef920570876955
outamaa <ville.outamaa@gmail.com>
2021-03-29 21:53:08 +0300

    Add pub uses

--- src/geometry/mod.rs
+++ src/geometry/mod.rs
@@ -1,4 +1,13 @@
 pub mod point;
+
+pub use point::Point2;
+pub use point::Point2f;
+pub use point::Point2i;
+
 pub use point::Point3;
 pub use point::Point3f;
 pub use point::Point3i;
+
+pub use point::Point4;
+pub use point::Point4f;
+pub use point::Point4i;
--- src/math/mod.rs
+++ src/math/mod.rs
@@ -1,2 +1,30 @@
 pub mod matrix;
 pub mod vec;
+
+pub use matrix::Matrix;
+
+pub use matrix::Matrix2;
+pub use matrix::Matrix2f;
+pub use matrix::Matrix2i;
+
+pub use matrix::Matrix3;
+pub use matrix::Matrix3f;
+pub use matrix::Matrix3i;
+
+pub use matrix::Matrix4;
+pub use matrix::Matrix4f;
+pub use matrix::Matrix4i;
+
+pub use matrix::Vector;
+
+pub use matrix::Vec2;
+pub use matrix::Vec2f;
+pub use matrix::Vec2i;
+
+pub use matrix::Vec3;
+pub use matrix::Vec3f;
+pub use matrix::Vec3i;
+
+pub use matrix::Vec4;
+pub use matrix::Vec4f;
+pub use matrix::Vec4i;
